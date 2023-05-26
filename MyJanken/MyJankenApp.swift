762c4f29f3a1a309452115bfbff9f4a9edbfe4cc
nogu66 <ezochi66@gmail.com>
2023-05-27 00:58:20 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by YutaNoguchi on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
